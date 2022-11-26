a471b48748a7382a65c7c3aa69d0a7a161332625
EC2 Default User <ec2-user@ip-172-31-23-29.us-east-2.compute.internal>
2022-11-26 08:53:18 +0000

    first commit

--- app.java
+++ app.java
@@ -1,4 +1,4 @@
-This is code for a web application
-Please do not hard code
-make use of dynamic variable
-author is simon legah
+This is a web application
+Do not hard code
+Use variables and dynamic variables
+Auther: Simon Legah
